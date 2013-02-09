09ce01693cff78d1c42e250ef8a9cf3a29369b7b
Paul Vorbach <paul@vorb.de>
2013-02-09 20:46:44 +0100

    Fix issue #1
    Correctly resolve image files

--- epub-en.sh
+++ epub-en.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
-pandoc --toc -S --epub-metadata=metadata.xml -o learn-you-a-haskell-en.epub title.txt \
-  en/01-introduction.md \
-  en/02-starting-out.md \
-  en/03-types-and-typeclasses.md \
-  en/04-syntax-in-functions.md \
-  en/05-recursion.md \
-  en/06-higher-order-functions.md \
-  en/07-modules.md \
-  en/08-making-our-own-types-and-typeclasses.md \
-  en/09-input-and-output.md \
-  en/10-functionally-solving-problems.md \
-  en/11-functors-applicative-functors-and-monoids.md \
-  en/12-a-fistful-of-monads.md \
-  en/13-for-a-few-monads-more.md \
-  en/14-zippers.md
+cd en/
+
+pandoc --toc -S --epub-metadata=../metadata.xml -o ../learn-you-a-haskell-en.epub ../title.txt \
+  01-introduction.md \
+  02-starting-out.md \
+  03-types-and-typeclasses.md \
+  04-syntax-in-functions.md \
+  05-recursion.md \
+  06-higher-order-functions.md \
+  07-modules.md \
+  08-making-our-own-types-and-typeclasses.md \
+  09-input-and-output.md \
+  10-functionally-solving-problems.md \
+  11-functors-applicative-functors-and-monoids.md \
+  12-a-fistful-of-monads.md \
+  13-for-a-few-monads-more.md \
+  14-zippers.md
+
+cd ../
